b8ef820283bdd936aa6182248c1808fb116ccbd2
Rohit <turkarcontact13@gmail.com>
2021-12-18 14:47:13 +0530

    update Question5

--- First-java/Question5.java
+++ First-java/Question5.java
@@ -1,5 +1,3 @@
-import javax.naming.ldap.ManageReferralControl;
-import javax.sound.sampled.SourceDataLine;
 
 //Take 2 numbers as input and print the largest number.
 import java.util.Scanner;
@@ -8,14 +6,13 @@ public class Question5 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         System.out.println("Enter the first number");
-        int a=in.nextInt();
+        int a = in.nextInt();
         System.out.println("Enter the second number");
-        int b=in.nextInt();
-        if(a>b){
-            System.out.println(a+"is largest number ");
-        }
-        else{
-            System.out.println(b+"is the largest number");
+        int b = in.nextInt();
+        if (a > b) {
+            System.out.println(a + "is largest number ");
+        } else {
+            System.out.println(b + "is the largest number");
         }
 
     }
