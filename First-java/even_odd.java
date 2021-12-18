0a35232621f53564cb34d4743c82023f7db99687
Rohit <turkarcontact13@gmail.com>
2021-12-18 10:11:43 +0530

    Update even_odd.java

---
+++ First-java/even_odd.java
@@ -0,0 +1,21 @@
+//Write a program to print whether a number is even or odd, also take input.
+import java.util.Scanner;
+
+
+
+
+
+class even_odd{
+    public static void main(String[] args) {
+       Scanner in=new Scanner(System.in);
+   System.out.println("Enter the number");
+   int num=in.nextInt();
+   if(num%2==0){
+       System.out.println("the number is Even");
+   }
+   else{
+       System.out.println("the number is Odd");
+   }
+
+    }
+}
